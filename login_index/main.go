ee5d7ccbbe11ffd272005ec52cad6d9d88552a9d
DeepakRamanan1997 <131782451+DeepakRamanan1997@users.noreply.github.com>
2024-08-21 20:25:25 +0530

    Update main.go

--- login_index/main.go
+++ login_index/main.go
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <title>Login Successful</title>
-        <style>
-                body {
-                        font-family: Arial, sans-serif;
-                        text-align: center;
-                }
-
-                .success-message {
-                        color: #3498DB; /* updated color code */
-                        font-size: 24px;
-                        margin-top: 50px;
-                }
-        </style>
-</head>
-<body>
-        <div class="success-message">
-                <h1>Login Successful!</h1>
-                <p>Welcome to our application. You have successfully logged in.</p>
-        </div>
-</body>
-</html>
-root@ip-172-31-60-105:/home/ubuntu/blue# cat main.go
 package main
 
 import (
